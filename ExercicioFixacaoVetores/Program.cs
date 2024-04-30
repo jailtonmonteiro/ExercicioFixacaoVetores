36eee33d8bc2730a8bcefdb0864c8050dedbc156
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2024-04-30 12:20:53 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioFixacaoVetores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace ExercicioFixacaoVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student[] students = new Student[9];
+
+            string name, email;
+            int room, qt;
+
+            Console.Write("QUANTIDADE DE ESTUDANTES: ");
+            qt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qt; i++)
+            {
+                Console.Write("NOME: ");
+                name = Console.ReadLine();
+
+                Console.Write("EMAIL: ");
+                email = Console.ReadLine();
+
+                Console.Write("QUARTO: ");
+                room = int.Parse(Console.ReadLine());
+
+                students[room] = new Student { Room = room, Email = email, Name = name };
+
+            }
+
+            for (int j = 0; j <= 8; j++)
+            {
+                if (students[j] != null)
+                {
+                    Console.WriteLine($"Quarto: {students[j].Room}");
+                    Console.WriteLine($"Nome: {students[j].Name}");
+                    Console.WriteLine($"Email: {students[j].Email}");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ ExercicioFixacaoVetores/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioFixacaoVetores
+{
+    internal class Student
+    {
+        string name;
+        string email;
+        int room;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int Room
+        {
+            get { return room; }
+            set { room = value; }
+        }
+    }
+}
